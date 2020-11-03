453444f589645e3b2a1637158b7bf0134ed6f93e
Michela Biale <mbiale@Michela-Biales-MacBook-Pro.local>
2020-11-03 14:20:58 -0800

    completed deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -28,3 +28,24 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+--Deliverable 2
+--mentorship_eligibility
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO mentorship_eligibility
+FROM employees as e 
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tt
+ON (e.emp_no = tt.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	 AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC 
+
